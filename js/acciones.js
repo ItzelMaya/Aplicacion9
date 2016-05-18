8a030c8de73939bbfcb6166646929e03ebec0eee
ItzelMaya <itzeldsaupoo@gmail.com>
2016-05-18 09:09:33 -0500

    subida4

--- js/acciones.js
+++ js/acciones.js
@@ -45,13 +45,13 @@
  function Localiza(posicion){
 	 var element=document.getElementById('geolocalizacion');
 	 element.innerHTML='Latitud: '+posicion.coords.latitude+'<br/>'+ 
-	 'Longitud: '+posicion.coords.longitude+'<br/>' +
-	 'Precision: '+posicion.coords.accuracy+'<br/>' +
-	 'Intervalo: '+posicion.timestamp +'<br/>';
+	 'Longitud:'+posicion.coords.longitude+'<br/>' +
+	 'Precision:'+posicion.coords.accuracy+'<br/>' +
+	 'Intervalo:'+posicion.timestamp +'<br/>';
  }
  //Error en la geolocalizacion 
  function ErrorLocalizacion(error){
-	 alert('codigo: '+error.code+'\n'+
-	 'mensaje: ' +error.message+'\n');
+	 alert('codigo:'+error.code+'\n'+
+	 'mensaje:'+error.message+'\n');
  }
 }); //document ready
