461f7ff621451888f65b0b30771a5999ccc8cd7b
ItzelMaya <itzeldsaupoo@gmail.com>
2016-05-18 08:44:35 -0500

    subida1

---
+++ js/acciones.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+ $(document).ready(function(e) {
+    //WatchID se refier a actual
+	
+ var WatchID=null;
+ document.addEventListener("deviceready",Dispositivo_Listo.false);
+ 
+ //Cuando esta listo el dispositivo
+ function Dispositivo_Listo(){
+	 comienza();
+ }
+ 
+ //Empieza la observacion de la aceleracion
+ function Comienza(){
+	 
+ //Actualizar la aceleracion cada 2segundos
+ //
+ var opciones={frecuency:2000};
+ 
+ WatchID=navigator.accelerometer.watchAcceleration(Correcto, Error, Opciones);
+ navigator.geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
+ }
+ //Detiene la aceleracion 
+ 
+ function Detente(){
+	 if(watchID){
+		 navigator.accelerometer.clearWatch (watchID);
+		 watchID=null;
+	 }
+ }
+ //correcto:Toma una captura de la aceleracion
+ function Correcto(acceleration){
+	 var element=document.getElementById('acelerometro');
+	 
+ element.innerHTML='Aceleracion en X:'+acceleration.x+'<br/>'+
+ 'Aceleracion en Y:'+acceleration.y+'<br/>'+
+ 'Intervalo:'+acceleration.timestamp+'<br/>';
+ }
+ 
+ //Error:Falla al obtener la aceleracion
+ function Error(){
+	 alert('Error!');
+ }
+ //Exito al Localizar
+ function Localiza(posicion){
+	 var element=document.getElementById('geolocalizacion');
+	 element.innerHTML='Latitud: '+posicion.coords.latitude +'<br/>'+ 
+	 'Longitud: ' +posicion.coords.longitude +'<br/>' +
+	 'Precision: ' +posicion.coords.accuracy +'<br/>' +
+	 'Intervalo: ' +posicion.timestamp +'<br/>';
+ }
+ //Error en la geolocalizacion 
+ function ErrorLocalizacion(error){
+	 alert('codigo: ' +error.code +'\n'+
+	 'mensaje ' +error.message+'\n');
+ }
+});
